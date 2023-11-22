2af52d1265366193b3b1caa012994c58b225c0e3
Rafael <122636087+Cr0nik@users.noreply.github.com>
2023-11-22 15:52:27 -0300

    Update funcoes.py

--- Infinity school/Aula16/funcoes.py
+++ Infinity school/Aula16/funcoes.py
@@ -25,4 +25,31 @@ class Flashinha():
                             """)
         self.conexao.commit()
         
+    def atualizar_dados(self, table_name, column_name, *values):
+        self.conectar()
+        self.cursor.execute(f"""UPDATE {table_name} SET {column_name} VALUES {values}
+                            """)
+        self.conexao.commit()
         
+    def excluir_dados(self, table_name, column_name, *values):
+        self.conectar()
+        self.cursor.execute(f"""DELETE FROM {table_name} {column_name}VALUES {values}
+                            """)
+        self.conexao.commit()
+        
+    def cadastro(self, *values):
+        self.conectar()
+        self.cursor.execute("""
+            INSERT INTO clientes (nome, email, cidade, estado, telefone, saldo) 
+            VALUES (?, ?, ?, ?, ?, ?);
+            """, values)
+        self.conexao.commit()
+        
+    def cadastro_multiplo(self, usuarios):
+        self.conectar()
+        usuarios_validos = [usuario for usuario in usuarios if len(usuario) == 6]
+        self.cursor.executemany("""
+            INSERT INTO clientes (nome, email, cidade, estado, telefone, saldo) 
+            VALUES (?, ?, ?, ?, ?, ?);
+        """, usuarios_validos)
+        self.conexao.commit()
